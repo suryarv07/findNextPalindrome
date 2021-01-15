6aa1125f15d2f03497804d5e9efcc3ce6d7e0d4e
Surya R <suryarv43@gmail.com>
2021-01-14 22:58:45 -0800

    first commit

---
+++ Getnextpalindrome.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+
+
+int getNextpalindrome(int n)
+{
+    int ans =1,digits,rev=0,number;
+
+    if (n<10)
+    {
+        ans=0;
+        return n+1;
+    }
+    number=n;
+    while(ans!=0)
+    {
+        rev=0;
+        digits=0;
+        n=++number;
+        while(n>0)
+        {
+            digits=n%10;
+            rev=rev*10+digits;
+            n=n/10;
+        }
+        if(rev==number)
+        {
+            ans=0;
+            return number;
+        }
+        else
+            ans=1;
+    }
+    return number;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter The Number  : ");
+    scanf("%d",&n);
+    if(n>0);
+    {
+        printf("%d",getNextpalindrome(n));
+    }
+}
+
